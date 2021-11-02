6b1c1d29b34c481386f68fcb3e15c74d5ff8ac81
Rander2 <rk.319100@gmail.com>
2021-11-02 18:54:13 +0300

    блять нахуй

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -1,25 +1,10 @@
 from django.urls import path
 
-from mainapp.views import cruise_list, liner_list, excursion_list, add_cruise, add_liner, add_excursion, edit_cruise, \
-    edit_liner, edit_excursion, delete_cruise, delete_liner, delete_excursion, exit_view, register
+from mainapp.views import exit_view, register, home
 
 urlpatterns = [
     path('exit', exit_view),
     path('accounts/register', register),
 
-    path('cruise/list/', cruise_list),
-    path('liner/list/', liner_list),
-    path('excursion/list/', excursion_list),
-
-    path('cruise/new/', add_cruise.as_view()),
-    path('liner/new/', add_liner.as_view()),
-    path('excursion/new/', add_excursion.as_view()),
-
-    path('cruise/<int:pk>/update', edit_cruise.as_view()),
-    path('liner/<int:pk>/update', edit_liner.as_view()),
-    path('excursion/<int:pk>/update', edit_excursion.as_view()),
-
-    path('cruise/<int:pk>/delete', delete_cruise.as_view()),
-    path('liner/<int:pk>/delete', delete_liner.as_view()),
-    path('excursion/<int:pk>/delete', delete_excursion.as_view()),
+    path('home', home)
 ]
--- mainapp/views.py
+++ mainapp/views.py
@@ -9,7 +9,7 @@ from mainapp.models import Cruise, Excursion, Liner
 
 def exit_view(request):
     logout(request)
-    return redirect('/liner/list')
+    return redirect('/home')
 
 
 def register(response):
@@ -17,7 +17,7 @@ def register(response):
         form = RegisterForm(response.POST)
         if form.is_valid():
             form.save()
-            return redirect('/liner/list')
+            return redirect('/home')
         else:
             form = RegisterForm()
             return render(response, "registration/registration.html", {"form": form})
@@ -26,88 +26,5 @@ def register(response):
     return render(response, "registration/registration.html", {"form": form})
 
 
-def cruise_list(request):
-    access = False
-    if request.user.is_authenticated:
-        if request.user.is_superuser or request.user.is_staff:
-            access = True
-    context = {"dataset": Cruise.objects.all(), "access": access}
-    return render(request, 'cruise_list.html', context)
-
-
-def liner_list(request):
-    access = False
-    if request.user.is_authenticated:
-        if request.user.is_superuser or request.user.is_staff:
-            access = True
-    context = {"dataset": Liner.objects.all(), "access": access}
-    return render(request, 'liner_list.html', context)
-
-
-def excursion_list(request):
-    access = False
-    if request.user.is_authenticated:
-        if request.user.is_superuser or request.user.is_staff:
-            access = True
-    context = {"dataset": Excursion.objects.all(), "access": access}
-    return render(request, 'excursion_list.html', context)
-
-
-class add_cruise(CreateView):
-    model = Cruise
-    template_name = 'add_cruise.html'
-    success_url = 'http://127.0.0.1:8000/cruise/list'
-    fields = ['name', 'days', 'length', 'stops']
-
-
-class add_liner(CreateView):
-    model = Liner
-    template_name = 'add_liner.html'
-    success_url = 'http://127.0.0.1:8000/liner/list'
-    fields = ['name', 'number', 'team', 'tickets']
-
-
-class add_excursion(CreateView):
-    model = Excursion
-    template_name = 'add_excursion.html'
-    success_url = 'http://127.0.0.1:8000/excursion/list'
-    fields = ['date', 'tickets', 'cruise', 'liner']
-
-
-class edit_cruise(UpdateView):
-    model = Cruise
-    fields = ['name', 'days', 'length', 'stops']
-    success_url = 'http://127.0.0.1:8000/cruise/list'
-    template_name = 'edit_cruise.html'
-
-
-class edit_liner(UpdateView):
-    model = Liner
-    fields = ['name', 'number', 'team', 'tickets']
-    success_url = 'http://127.0.0.1:8000/liner/list'
-    template_name = 'edit_liner.html'
-
-
-class edit_excursion(UpdateView):
-    model = Excursion
-    fields = ['date', 'tickets', 'cruise', 'liner']
-    success_url = 'http://127.0.0.1:8000/excursion/list'
-    template_name = 'edit_excursion.html'
-
-
-class delete_liner(DeleteView):
-    model = Liner
-    template_name = 'delete_object.html'
-    success_url = "http://127.0.0.1:8000/liner/list"
-
-
-class delete_cruise(DeleteView):
-    model = Cruise
-    template_name = 'delete_object.html'
-    success_url = "http://127.0.0.1:8000/cruise/list"
-
-
-class delete_excursion(DeleteView):
-    model = Excursion
-    template_name = 'delete_object.html'
-    success_url = "http://127.0.0.1:8000/excursion/list"
+def home(response):
+    return render(response, "home.html")
